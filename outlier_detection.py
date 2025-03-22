dbfce826b153b22c7376e80045b17bbb4a1c749c
Arianna Pryor Kernosek <57537305+studious-broccoli@users.noreply.github.com>
2025-03-22 11:14:45 -0600

    Initial mahal distance calculation
    Initial mahal distance calculation

---
+++ outlier_detection.py
@@ -0,0 +1,66 @@
+import os
+import pdb
+import pandas as pd
+import mlflow
+import click
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import numpy as np
+from scipy.stats import chi2
+from scipy.spatial.distance import mahalanobis
+
+
+def plot_outliers(data, mahalanobis_dist, save_name):
+    norm = plt.Normalize(min(mahalanobis_dist), max(mahalanobis_dist))
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111, projection='3d')
+    sc = ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=mahalanobis_dist, cmap="coolwarm", norm=norm, label='Mahalanobis distances')
+    ax.set_title("Mahalanobis Distance (PCA-TSNE Components)")
+    ax.set_xlabel('Component 1')
+    ax.set_ylabel('Component 2')
+    ax.set_zlabel('Component 3')
+    plt.legend()
+    cbar = plt.colorbar(sc, ax=ax)
+    plt.savefig(save_name)
+    plt.close()
+
+
+def calculate_mahal(data, y, alpha=0.05):
+    total_outliers = np.zeros(len(y))  # Outlier flags (0 = not outlier, 1 = outlier)
+    total_distances = np.zeros(len(y))  # Mahalanobis distances
+
+    unique_classes = np.unique(y)  # Get unique classes
+
+    for unique_class in unique_classes:
+        sub_data = data[y == unique_class]
+
+        mean = np.mean(sub_data, axis=0)
+        try:
+            cov = np.cov(sub_data.T)
+            inv_cov = np.linalg.inv(cov)
+        except np.linalg.LinAlgError:
+            print(f"Warning: Singular covariance matrix for class {unique_class}. Skipping outlier detection.")
+            continue
+
+        mahalanobis_dist = np.array([mahalanobis(x, mean, inv_cov) for x in sub_data])
+
+        C = np.sqrt(chi2.ppf(1 - alpha, df=sub_data.shape[1]))  # Cut-off threshold
+
+        outliers = (mahalanobis_dist > C).astype(int)
+
+        total_outliers[y == unique_class] = outliers
+        total_distances[y == unique_class] = mahalanobis_dist
+
+    return total_distances, total_outliers
+
+
+def pca_data(data, n_components=5):
+    pca = PCA(n_components=n_components)
+    data_pca = pca.fit_transform(data)
+    return data_pca
+
+def tsne_data(data, n_components=3):
+    tsne = TSNE(n_components=n_components, random_state=42)
+    data_3d = tsne.fit_transform(data)
+    return data_3d
