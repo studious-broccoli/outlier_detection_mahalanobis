bc70d151b8713c4854b267e2755e27b80b536031
studious-broccoli <ariannampryor@gmail.com>
2025-03-25 20:43:00 -0600

    added data for outlier plot

--- outlier_detection.py
+++ outlier_detection.py
@@ -1,8 +1,5 @@
-import os
 import pdb
 import pandas as pd
-import mlflow
-import click
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -11,6 +8,9 @@ from scipy.stats import chi2
 from scipy.spatial.distance import mahalanobis
 
 
+# -----------------------------------------------------------------------------------------------
+# Plot function
+# -----------------------------------------------------------------------------------------------
 def plot_outliers(data, mahalanobis_dist, save_name):
     norm = plt.Normalize(min(mahalanobis_dist), max(mahalanobis_dist))
     fig = plt.figure(figsize=(10, 7))
@@ -26,6 +26,9 @@ def plot_outliers(data, mahalanobis_dist, save_name):
     plt.close()
 
 
+# -----------------------------------------------------------------------------------------------
+# Calculate
+# -----------------------------------------------------------------------------------------------
 def calculate_mahal(data, y, alpha=0.05):
     total_outliers = np.zeros(len(y))  # Outlier flags (0 = not outlier, 1 = outlier)
     total_distances = np.zeros(len(y))  # Mahalanobis distances
@@ -55,7 +58,10 @@ def calculate_mahal(data, y, alpha=0.05):
     return total_distances, total_outliers
 
 
-def pca_data(data, n_components=5):
+# -----------------------------------------------------------------------------------------------
+# Data dimensionality reduction
+# -----------------------------------------------------------------------------------------------
+def pca_data(data, n_components=15):
     pca = PCA(n_components=n_components)
     data_pca = pca.fit_transform(data)
     return data_pca
@@ -64,3 +70,37 @@ def tsne_data(data, n_components=3):
     tsne = TSNE(n_components=n_components, random_state=42)
     data_3d = tsne.fit_transform(data)
     return data_3d
+
+# -----------------------------------------------------------------------------------------------
+# Dummy data (Kaggle Cancer)
+# -----------------------------------------------------------------------------------------------
+df = pd.read_csv("data.csv")
+print(df.head(2))
+print(df.columns)
+print(df.shape)
+
+
+# -----------------------------------------------------------------------------------------------
+# Format data
+# -----------------------------------------------------------------------------------------------
+labels = df["diagnosis"]
+data = df.drop(['id', 'diagnosis'], axis=1)
+data.dropna(axis=1, inplace=True)
+print(labels.shape)
+print(data.shape)
+
+# -----------------------------------------------------------------------------------------------
+# Dimensionality reduction: Calculate PCA and TSNE Components
+# -----------------------------------------------------------------------------------------------
+data_pca = tsne_data(data)
+data_tsne = tsne_data(data_pca)
+
+# -----------------------------------------------------------------------------------------------
+# Calculate Mahalanobis Distance
+# -----------------------------------------------------------------------------------------------
+mahalanobis_dist, outliers = calculate_mahal(data_pca, labels)
+
+# -----------------------------------------------------------------------------------------------
+# Plot 3-D
+# -----------------------------------------------------------------------------------------------
+plot_outliers(data_tsne, mahalanobis_dist, "outliers.png")
